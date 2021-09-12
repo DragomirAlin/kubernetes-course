84de271aedb22722fc3b788b4d79210d836efe01
dragomiralin <dragomirdanielalin@Yahoo.com>
2021-09-12 20:23:33 +0300

    update

--- microservice/src/main/java/ro/dragomiralin/microservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.dragomiralin.microservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import ro.dragomiralin.microservice.model.Order;
-
-@Repository
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
